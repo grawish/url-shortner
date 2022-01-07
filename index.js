e20d2423100839f2dc3bf19298c16c6e547bfc28
grawish <grawish06@gmail.com>
2022-01-07 08:15:05 +0530

    push

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const app = require('express')();
-const bodyparser = require('body-parser');
+const express = require('express');
+const app = express();
 const validUrl = require('valid-url');
 const shortid = require('shortid');
 const { Urls } = require('./models');
 
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.send(`<pre>${JSON.stringify(req.headers, null, 2)}</pre>`);
@@ -14,40 +13,62 @@ app.get('/', async (req, res) => {
 app.post('/create', async (req, res) => {
   const { long_url, short, random } = req.body;
 
-  if (!validUrl.isUri(long_url)) {
-    res.send({ status: 'Error', message: 'Is Not A Valid Url!' });
-    return;
-  }
+  if (validUrl.isUri(long_url)) {
+    // url is valid
+    let count;
+    let short_url;
 
-  const count = await Urls.count({ where: { short_url: short } });
-
-  if (count && !random) {
-    res.send({ status: 'Error!', message: 'Already Exists!' });
-    return;
-  }
-
-  let short_url;
-
-  if (random) {
-    short_url = shortid.generate();
-    while (await Urls.count({ where: { short_url: short_url } })) {
-      short_url = shortid.generate();
+    if(random===false){
+      res.status(400).send(JSON.stringify({
+        status: "error",
+        message: "Invalid Request"
+      }))
     }
-  } else short_url = short;
+    if (!random) {
+      //not random
+      if (short) {
+        // short is there
+        count = await Urls.count({ where: { short_url: short } });
+        if (count && !random) {
+          res.status(409).send({ status: 'Error!', message: 'Already Exists!' });
+          return;
+        }
 
-  const variable = await Urls.create({
-    long_url,
-    short_url,
-  });
+        short_url = short;
+      } else {
+        // nor random neither short
+        // default random is true
+        short_url = shortid.generate();
+        while (await Urls.count({ where: { short_url: short_url } })) {
+          short_url = shortid.generate();
+        }
+      }
+    } else {
+      short_url = shortid.generate();
+      while (await Urls.count({ where: { short_url: short_url } })) {
+        short_url = shortid.generate();
+      }
+    }
 
-  if (variable) {
-    res.send({
-      status: 'success',
-      message: req.headers.host + '/' + short_url,
+    const variable = await Urls.create({
+      long_url,
+      short_url,
     });
-  } else {
-    res.send({ status: 'Error!', message: 'Unknown' });
+
+    if (variable) {
+      res.status(201).send({
+        status: 'success',
+        message: req.headers.host + '/' + short_url,
+      });
+      return;
+    } else {
+      res.status(500).send({ status: 'Error!', message: 'Unknown' });
+      return;
+    }
   }
+
+  res.status(400).send({ status: 'Error', message: 'Is Not A Valid Url!' });
+  return;
 });
 
 app.get('/:id', async (req, res) => {
@@ -63,7 +84,7 @@ app.get('/:id', async (req, res) => {
     },
   });
 
-  res.redirect(variable.long_url);
+  res.status(302).redirect(variable.long_url);
 });
 
 const PORT = process.env.PORT || 3000;
